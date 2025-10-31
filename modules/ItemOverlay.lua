762742706a114c35bce6c46a4ee6d1b1f06d5a3f
Slackluster <ruben.sluimer@gmail.com>
2025-10-31 20:22:49 +0100

    Don't query other addons more than needed

--- modules/ItemOverlay.lua
+++ modules/ItemOverlay.lua
@@ -330,6 +330,15 @@ function app.ItemOverlay(overlay, itemLink, itemLocation, containerInfo, bagAddo
 		if app.Icon[itemEquipLoc] then
 			-- Appearances
 			if TransmogLootHelper_Settings["iconNewMog"] and itemEquipLoc:find("INVTYPE") then
+				local attInfo
+				if C_AddOns.IsAddOnLoaded("AllTheThings") then
+					attInfo = AllTheThings.GetLinkReference(itemLink)
+				end
+				local tumInfo
+				if C_AddOns.IsAddOnLoaded("TransmogUpgradeMaster") then
+					tumInfo = TransmogUpgradeMaster_API.GetAppearanceMissingData(itemLink)
+				end
+
 				-- Legendaries and Artifacts can be a little weird
 				if (app.OverlayCache[itemLink].itemQuality == 5 or app.OverlayCache[itemLink].itemQuality == 6) and bindType == 1 then
 					if TransmogLootHelper_Settings["iconLearned"] then
@@ -344,17 +353,17 @@ function app.ItemOverlay(overlay, itemLink, itemLocation, containerInfo, bagAddo
 				elseif TransmogLootHelper_Settings["iconNewSource"] and not api.IsSourceCollected(itemLink) then
 					showOverlay("yellow")
 				-- Catalyst mog
-				elseif TransmogLootHelper_Settings["iconNewCatalyst"] and C_AddOns.IsAddOnLoaded("TransmogUpgradeMaster") and TransmogUpgradeMaster_API.GetAppearanceMissingData(itemLink).catalystAppearanceMissing then
+				elseif TransmogLootHelper_Settings["iconNewCatalyst"] and tumInfo and tumInfo.catalystAppearanceMissing then
 					overlay.texture:SetAtlas("CreationCatalyst-32x32")
 					showOverlay("yellow")
-				elseif TransmogLootHelper_Settings["iconNewCatalyst"] and C_AddOns.IsAddOnLoaded("AllTheThings") and AllTheThings.GetLinkReference and AllTheThings.GetLinkReference(itemLink) and AllTheThings.GetLinkReference(itemLink).filledCatalyst then
+				elseif TransmogLootHelper_Settings["iconNewCatalyst"] and attInfo and attInfo.filledCatalyst then
 					overlay.texture:SetTexture("Interface\\AddOns\\AllTheThings\\assets\\Interface_Catalyst")
 					showOverlay("yellow")
 				-- Upgrade mog
-				elseif TransmogLootHelper_Settings["iconNewUpgrade"] and C_AddOns.IsAddOnLoaded("TransmogUpgradeMaster") and (TransmogUpgradeMaster_API.GetAppearanceMissingData(itemLink).upgradeAppearanceMissing or TransmogUpgradeMaster_API.GetAppearanceMissingData(itemLink).catalystUpgradeAppearanceMissing) then
+				elseif TransmogLootHelper_Settings["iconNewUpgrade"] and tumInfo and tumInfo.catalystUpgradeAppearanceMissing then
 					overlay.texture:SetAtlas("CovenantSanctum-Upgrade-Icon-Available")
 					showOverlay("yellow")
-				elseif TransmogLootHelper_Settings["iconNewUpgrade"] and C_AddOns.IsAddOnLoaded("AllTheThings") and AllTheThings.GetLinkReference and AllTheThings.GetLinkReference(itemLink) and AllTheThings.GetLinkReference(itemLink).filledUpgrade then
+				elseif TransmogLootHelper_Settings["iconNewUpgrade"] and attInfo and attInfo.filledUpgrade then
 					overlay.texture:SetTexture("Interface\\AddOns\\AllTheThings\\assets\\Interface_Upgrade")
 					showOverlay("yellow")
 				-- Learned
