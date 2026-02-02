622984853f11a42a144ddd15173f76d8de95c5f3
Slackluster <29845810+Slackluster@users.noreply.github.com>
2026-02-02 05:38:06 +0100

    Remove the GitHub button from settings (those who would use it, can already find it)

--- Settings.lua
+++ Settings.lua
@@ -177,11 +177,6 @@ function app:CreateSettings()
 	end
 	layout:AddInitializer(CreateSettingsButtonInitializer(L.SETTINGS_HELP_TEXT, L.SETTINGS_HELP_BUTTON, onHelpButtonClick, L.SETTINGS_HELP_DESC, true))
 
-	local function onIssuesButtonClick()
-		StaticPopup_Show("TRANSMOGLOOTHELPER_URL", nil, nil, "https://github.com/slackluster/TransmogLootHelper/issues")
-	end
-	layout:AddInitializer(CreateSettingsButtonInitializer(L.SETTINGS_ISSUES_TEXT, L.SETTINGS_ISSUES_BUTTON, onIssuesButtonClick, L.SETTINGS_ISSUES_DESC, true))
-
 	TransmogLootHelper_SettingsExpandMixin = CreateFromMixins(SettingsExpandableSectionMixin)
 
 	function TransmogLootHelper_SettingsExpandMixin:Init(initializer)
