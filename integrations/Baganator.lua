87f6a87c4b0c87c266dda2468cb68a54c14b387d
Slackluster <29845810+Slackluster@users.noreply.github.com>
2026-08-14 16:56:36 +0200

    Fix Baganator bank lag

--- integrations/Baganator.lua
+++ integrations/Baganator.lua
@@ -11,7 +11,6 @@ app.Event:Register("ADDON_LOADED", function(addOnName, containsBindings)
 						return
 					end
 					app:ApplyItemOverlay(icon.overlay, itemDetails.itemLink, nil, { hasLoot = itemDetails.hasLoot }, true)
-					Baganator.API.RequestItemButtonsRefresh()
 					return icon:IsShown()
 				end,
 				function(itemButton)
